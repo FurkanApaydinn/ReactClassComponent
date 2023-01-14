360d30540d0d99cd596e552beac0d0bfad6e1b6a
Furkan Apaydin <furkanapydn@gmail.com>
2023-01-14 14:19:17 +0300

    filter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from './logo.svg'
+import './App.css'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      canavarlar: [],
+      aramaAlanı: '',
+    }
+    console.log('constructor')
+  }
+  componentDidMount() {
+    console.log('componentDidMount')
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { canavarlar: users }
+          },
+          () => {
+            console.log(this.state)
+          }
+        )
+      )
+  }
+
+  aramaDeğişikliği = (e) => {
+    const aramaAlanı = e.target.value.toLocaleLowerCase()
+
+    this.setState(() => {
+      return { aramaAlanı }
+    })
+  }
+
+  render() {
+    console.log('render')
+    const { canavarlar, aramaAlanı } = this.state
+    const { aramaDeğişikliği } = this
+    const filtrelenmişCanavarlar = canavarlar.filter(
+      //[{name: "Leanne"}, {name: "Emre"}]
+      (canavar) => {
+        return canavar.name.toLocaleLowerCase().includes(aramaAlanı)
+      }
+    )
+    return (
+      <div className='App'>
+        <input
+          className='search-box'
+          type='search'
+          placeholder='search-monsters'
+          onChange={aramaDeğişikliği}
+        />
+        {filtrelenmişCanavarlar.map((canavar) => {
+          return (
+            <div key={canavar.id}>
+              <h1>{canavar.name}</h1>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
